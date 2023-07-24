9d66cec122283fe25c706229a2ab8d507f357157
Oliver <95024811+samaradeoliveira@users.noreply.github.com>
2023-07-24 15:25:07 -0300

    Add files via upload

---
+++ link.js
@@ -0,0 +1,14 @@
+class Link {
+    //bodyA corpo da corda
+    //bodyBcorpo da fruta
+    
+        //var para receber o ultimo elemento do corpo da corda
+        
+
+        // a restrição
+       
+       
+    }
+
+   
+
